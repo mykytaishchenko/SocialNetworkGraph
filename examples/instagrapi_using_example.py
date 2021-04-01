16e87e4e420b3ae181c9d95add0a17c49fb589c0
Nikita Ishchenko <mykyta.ishchenko@ucu.edu.ua>
2021-04-01 20:57:58 +0300

    instagram api using example upload

---
+++ examples/instagrapi_using_example.py
@@ -0,0 +1,27 @@
+"""A module for demonstrating the capabilities of the
+library for receiving data from Instagram."""
+
+from instagrapi import Client
+
+
+LOGIN = "Insert your Instagram username here."
+PASSWORD = "Insert your Instagram password here."
+
+
+if __name__ == "__main__":
+    cl = Client()  # Creating new Instagram client (user)
+    cl.login(LOGIN, PASSWORD)  # Login in Instagram account
+
+    user_info = cl.user_info_by_username(LOGIN)  # Getting information about user by username
+    user_followers = cl.user_followers(cl.user_id_from_username(LOGIN))  # Getting user followers
+
+    print(f"User name: {user_info.full_name}")
+    print(f"User username: {user_info.username}")
+    print(f"User id: {user_info.pk}")
+    print(f"User biography: {user_info.biography}")
+    print(f"User followers: {user_info.follower_count}")
+    print(f"User following: {user_info.following_count}")
+    print(f"The number of the user posts: {user_info.media_count}")
+    print(f"User photo: {user_info.profile_pic_url}")
+    print(f"20 user followers usernames: "
+          f"{', '.join([user_followers[key].username for key in user_followers.keys()][:20])}.")
